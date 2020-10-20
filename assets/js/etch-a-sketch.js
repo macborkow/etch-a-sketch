b6d75313b6c04912dbae590e549eb5259b7485f8
Maciej Borkowski <33153765+macborkow@users.noreply.github.com>
2020-10-20 13:44:07 +0200

    Add files via upload

--- assets/js/etch-a-sketch.js
+++ assets/js/etch-a-sketch.js
@@ -1,1 +1,33 @@
+const canvas = document.querySelector('#canvas');
+const button = document.querySelector('#drawGrid');
+let grid = [];
 
+function drawGrid(size) {
+	grid.forEach(function(item) {
+		item.remove();
+	});
+	for(let i = 0; i < size*size; ++i) {
+		grid[i] = document.createElement('div');
+		grid[i].classList.add('square');
+		grid[i].style.width = `calc(-2px + 40vw/${size})`;
+		grid[i].style.height = `calc(-2xp + 40vw/${size})`;
+		canvas.style.width = grid[i].style.width*size;
+		canvas.style.height = grid[i].style.width*size;
+		grid[i].addEventListener('mouseenter', () => { grid[i].classList.add('colored'); });
+		canvas.appendChild(grid[i]);
+	}
+}
+
+function reset() {
+	let size = 0;
+	
+	do {
+		size = parseInt(prompt("Pick grid density [1-100]"), 10);
+} while(size > 100 || size < 1);
+	
+	drawGrid(size);
+}
+
+document.onload = drawGrid(16);
+
+button.addEventListener('click', () => reset());
